680654f8ab72bccb299d34c25d52466cf5590f5d
mytychmarcin <111054155+mytychmarcin@users.noreply.github.com>
2025-04-02 15:12:22 +0000

    zad3_4ok

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,13 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Random;
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    public static void main(String[] args) {
+        Random random = new Random();
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.println( + i +  + j +   + random.nextInt(101));
+            }
+        }
+    }
 }
